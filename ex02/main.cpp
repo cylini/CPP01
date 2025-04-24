073f149ada0cba5edbbf125cc60482213cd2d4b0
carzhang <carzhang@student.42.fr>
2025-04-24 13:36:56 +0200

    exo1 ok, exo2 ok

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+int main(void)
+{
+	std::string sentence = "HI THIS IS BRAIN";
+	std::string *stringPTR = &sentence;
+	std::string &stringREF = sentence;
+
+	std::cout << "The memory address of the string variable: " << &sentence << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "The value of the string variable: " << sentence << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
