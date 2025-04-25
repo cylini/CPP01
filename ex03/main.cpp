0d9b8e8c89b9b87e5ef630d1ecfe1dc25fe5c33b
carzhang <carzhang@student.42.fr>
2025-04-26 00:05:29 +0200

    exo03 ok

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,18 +4,18 @@
 
 int main()
 {
-	Weapon club("bomb");
+	Weapon club("bomb"); // donne le nom de l'arme
 
-	HumanA bob("Boby", club);
-	bob.attack(); // Bob attacks with their crude spiked club
-	club.setType("some other type of club");
-	bob.attack(); // Bob attacks with their some other type of club
+	HumanA bob("Boby", club);				 // donne le nom de la personne et l'arme
+	bob.attack();							 // Bob attacks with their crude spiked club
+	club.setType("some other type of club"); // lui change le nom de l'arme
+	bob.attack();							 // Bob attacks with their some other type of club
 
-	Weapon knife("sharp knife");
-	HumanB jim("Jimy");
-	jim.attack(); // Jim has no weapon
-	jim.setWeapon(knife);
-	jim.attack(); // Jim attacks with their sharp knife
-	knife.setType("butter knife");
-	jim.attack(); // Jim attacks with their butter knife
+	Weapon knife("sharp knife");   // donne le nom de l'arme
+	HumanB jim("Jimy");			   // donne le nom de la personne
+	jim.attack();				   // Jim has no weapon
+	jim.setWeapon(knife);		   // donne l'arme à la personne
+	jim.attack();				   // Jim attacks with their sharp knife
+	knife.setType("butter knife"); // lui change le nom de l'arme
+	jim.attack();				   // Jim attacks with their butter knife
 }
